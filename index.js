b636cde220b2887e57e2e2a61dab03403b47d9a6
John Burnette <john.burnette@revcontent.com>
2023-05-11 13:26:33 -0400

    Update action to allow consuming a remotely fetched list

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const actionsCore = require('@actions/core');
 const fetch = require('node-fetch');
 
-const ENDPOINT = `https://gateway.stackpath.com/identity/v1/oauth2/token`;
+const AUTH_ENDPOINT = `https://gateway.stackpath.com/identity/v1/oauth2/token`;
+const PURGE_ENDPOINT = `https://gateway.stackpath.com/cdn/v1/stacks`;
+
+const PURGE_TYPES = {
+  LIST: 'list',
+  FETCH: 'fetch'
+};
 
 (async () => {
   try {
-    const clientId = actionsCore.getInput('clientId'),
-      clientSecret = actionsCore.getInput('clientSecret'),
-      stackId = actionsCore.getInput('stackId'),
-      urls = actionsCore.getInput('urls').split('\n').map(url => url.trim());
+    const clientId = actionsCore.getInput('clientId');
+    const clientSecret = actionsCore.getInput('clientSecret');
 
-    const auth = await fetch (ENDPOINT, {
+    if (!clientId || !clientSecret)
+      throw new Error(
+        'You must supply a valid clientId and clientSecret in your workflow'
+      );
+
+    const stackId = actionsCore.getInput('stackId');
+
+    if (!stackId)
+      throw new Error('You must supply a valid stackId in your workflow');
+
+    const purgeType = actionsCore.getInput('purgeType') || PURGE_TYPES.LIST;
+
+    const auth = await fetch(AUTH_ENDPOINT, {
       method: 'POST',
       headers: {
         'content-type': 'application/json'
@@ -20,20 +36,43 @@ const ENDPOINT = `https://gateway.stackpath.com/identity/v1/oauth2/token`;
         client_secret: clientSecret,
         grant_type: 'client_credentials'
       })
-    })
+    });
 
     const response = await auth.json();
     const accessToken = response.access_token;
 
-    const urlMap = [...urls].map(entry => ({
-      url: entry,
-      recursive: true,
-      invalidateOnly: true,
-      purgeAllDynamic: true
-    }))
+    const urlInput = actionsCore.getInput('urls');
+    const branch = actionsCore.getInput('branch') || 'main';
+    let urlMap;
+    if (purgeType === PURGE_TYPES.FETCH) {
+      const domainsResponse = await fetch(urlInput).then(res => res.json());
+      const urlList = [];
+      domainsResponse.forEach(domain => {
+        urlList.push(`${domain}/${branch}/`);
+      });
 
+      urlMap = [...urlList].map(url => ({
+        url,
+        recursive: true,
+        invalidateOnly: true,
+        purgeAllDynamic: true
+      }));
+    } else {
+      urlMap = [...urls]
+        .split('\n')
+        .map(url => url.trim())
+        .map(url => ({
+          url,
+          recursive: true,
+          invalidateOnly: true,
+          purgeAllDynamic: true
+        }));
+    }
 
-    const purge = await fetch(`https://gateway.stackpath.com/cdn/v1/stacks/${stackId}/purge`, {
+    console.log(`Purging URLs from Stackpath:`);
+    console.log(urlMap);
+
+    const purge = await fetch(`${PURGE_ENDPOINT}/${stackId}/purge`, {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
@@ -43,17 +82,14 @@ const ENDPOINT = `https://gateway.stackpath.com/identity/v1/oauth2/token`;
       body: JSON.stringify({
         items: urlMap
       })
-    })
+    });
 
     const purgeResponse = await purge.json();
 
-    console.log("Response from purge call: ", purgeResponse);
+    console.log('Response from purge call: ', purgeResponse);
     console.log(`Purge Request ID: ${purgeResponse.id}`);
     actionsCore.setOutput('Purge Request ID', purgeResponse.id);
-
-  }
-  catch (err) {
+  } catch (err) {
     actionsCore.setFailed(err.message);
   }
 })();
-
